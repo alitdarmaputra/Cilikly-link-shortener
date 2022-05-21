8c6b5da7b2f166e2166ce5a6f61e0afd16f6291e
alitdarmaputra <alitdarmaputra@gmail.com>
2022-05-21 11:40:38 +0800

    Create connection to mysql database

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
-
+const mysql = require("mysql2");
 const app = express();
+const { env } = require("process");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./web/views"));
 app.use(express.static(path.resolve(__dirname + "/web/public")));
+
+// Database connect
+const db_config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "cilickly_db"
+}
+
+const connection = mysql.createConnection(db_config)
 
 // Controllers
 const homePageController = require("./controller/homepageController.js");
