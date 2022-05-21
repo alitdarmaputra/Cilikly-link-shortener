8bfeeb8e2c1811901f5034ce570cbcb3b596cc2b
alitdarmaputra <alitdarmaputra@gmail.com>
2022-05-21 10:12:43 +0800

    [#1]Create initial front end

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const path = require("path");
 const ejs = require("ejs");
 
 const app = express();
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./web/views"));
-
 app.use(express.static(path.resolve(__dirname + "/web/public")));
+
+// Controllers
+const homePageController = require("./controller/homepageController.js");
+const loginController = require("./controller/loginController.js");
+const signupController = require("./controller/signupController.js");
 // Route
-app.get("/", (req, res) => {
-    res.render("index");
-});
+app.get("/", homePageController);
+app.get("/login", loginController);
+app.get("/signup", signupController);
 
 app.listen(process.env.PORT, () => {
     console.log(`[${new Date().toLocaleString()}]: Cilikly server listen on port ${process.env.PORT}`);
