d5974c614ec53c4d71af3b5496422b29b2436a0e
alitdarmaputra <alitdarmaputra@gmail.com>
2022-05-28 13:23:20 +0800

    Create modal to create-link

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,45 @@
 const accountBtn = document.querySelector(".account-wrapper");
 const accountModal = document.querySelector(".account-modal-container");
+const createLinkBtn = document.querySelector(".create-link-button");
+const createLinkModal = document.querySelector(".create-link-modal-container");
+const closeLinkModal = document.querySelector(".create-link-modal-close");
+const overlay = document.querySelector(".overlay");
 
-accountBtn.addEventListener("click", () => {
-    const classList = ["hidden", "flex", "top-10", "top-20"];
+function showOverlay() {
+    overlay.classList.toggle("hidden");
+    overlay.classList.toggle("opacity-0");
+    overlay.classList.toggle("opacity-70");
+}
+
+function showCreateLinkModal() {
+    createLinkModal.classList.toggle("-mr-72");
+}
+
+function showAccountModal() {
+    const classList = ["hidden", "flex", "top-20"];
     for(const iterator of classList) {
         accountModal.classList.toggle(iterator);
     }
+}
+
+// Accoount Modal
+accountBtn.addEventListener("click", () => {
+    showAccountModal();
+});
+
+// Create Link Modal
+createLinkBtn.addEventListener("click", () => {
+    showCreateLinkModal();
+    showOverlay();
+});
+
+closeLinkModal.addEventListener("click", () => {
+    showCreateLinkModal();
+    showOverlay();
+});
+
+// Overlay
+overlay.addEventListener("click", e => {
+    showCreateLinkModal();
+    showOverlay();
 })
