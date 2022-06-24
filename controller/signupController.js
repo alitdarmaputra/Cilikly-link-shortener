843ac8f41648a354479137ef170dc31d64d5a706
alitdarmaputra <alitdarmaputra@gmail.com>
2022-06-24 17:04:43 +0800

    Add validator at singup

--- controller/signupController.js
+++ controller/signupController.js
@@ -1,3 +1,16 @@
 module.exports = (req, res) => {
-    res.render("signup");
+	let email, username, password;
+	
+	if(req.flash("data")[0] != undefined) {
+		email, username, password = req.flash("data")[0];
+	}
+
+	if(username == undefined) username = "";
+	if(email == undefined) email = "";
+	if(password == undefined) password = "";
+
+	res.render("signup", {
+		error: req.flash("validationError"),
+		email, username, password
+	});
 };
--- controller/storeUserController.js
+++ controller/storeUserController.js
@@ -1,22 +1,38 @@
 const bcrypt = require("bcrypt");
 const connection = require("../models/mysqlConnect");
+const validator = require("validator");
 
 module.exports = async (req, res) => {
+	const error = [];
     let {email, username, password} = req.body;
+	req.flash("data", req.body);
 
-    const userExist = await connection.query(
-        `SELECT * FROM Users 
-        WHERE email = "${email}" OR 
-        username = "${username}";`);
+	if(email == "") error.push("Please provide email");
+	if(username == "") error.push("Please provide username");
+	if(password == "") error.push("Please provide password");
 
-    if(userExist.length != 0) {
-        res.redirect("/auth/signup");
-    } else {
-        password = await bcrypt.hash(password, 12);
-        await connection.query(`
-            INSERT INTO Users (email, username, password) 
-            VALUES ("${email}", "${username}", "${password}")`
-        );
-		res.redirect("/auth/login");
-    }
+	if(error.length > 0) {
+		req.flash("validationError", error);
+		res.redirect("/auth/signup");
+	} else if(!validator.isEmail(email)) {
+		req.flash("validationError", "Invalid email format");
+	}else {
+		const userExist = await connection.query(
+			`SELECT * FROM Users 
+			WHERE email = "${email}" OR 
+			username = "${username}";`);
+
+		if(userExist.length != 0) {
+			req.flash("validationError", "Use another email and username");
+			res.redirect("/auth/signup");
+		} else {
+			password = await bcrypt.hash(password, 12);
+			await connection.query(`
+				INSERT INTO Users (email, username, password) 
+				VALUES ("${email}", "${username}", "${password}")`
+			);
+			res.redirect("/auth/login");
+		}
+	}
+    
 }
